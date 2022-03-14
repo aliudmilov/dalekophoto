ec819dde1703627cfd2863927dfa5bcddb4318a9
Aleksandar Aleksandrov <aliudmilov@yahoo.com>
2022-03-14 19:29:55 +0000

    Added basic structure of the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AlbumComponent } from './album/album.component';
+import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationMenuComponent],
+  declarations: [AppComponent, NavigationMenuComponent, HomeComponent, GalleryComponent, AboutComponent, ContactUsComponent, AlbumComponent, PhotoComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
